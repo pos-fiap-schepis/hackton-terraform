95436b88370a4fd72ce63b9955d83e943a3e05a8
Guilherme Luz <>
2025-02-08 21:02:38 -0300

    fix: attach node group

--- clouds/aws/modules/vpc/main.tf
+++ clouds/aws/modules/vpc/main.tf
@@ -9,10 +9,10 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)
-
+  count = 3
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
+  cidr_block              = element(["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = merge(var.tags, {
@@ -21,11 +21,11 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidrs)
+  count = 3
 
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zone[count.index]
+  cidr_block        = element(["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"], count.index)
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 
   tags = merge(var.tags, {
     Name = "${var.name}-private-${count.index}"
@@ -43,19 +43,50 @@ resource "aws_internet_gateway" "this" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
   tags = merge(var.tags, {
     Name = "${var.name}-public-rt"
   })
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
-
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_association" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "eks-nat-gateway"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "eks-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  count     = 3
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
--- clouds/aws/modules/vpc/variables.tf
+++ clouds/aws/modules/vpc/variables.tf
@@ -8,22 +8,6 @@ variable "cidr" {
   type        = string
 }
 
-variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-}
-
-variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-}
-
-variable "availability_zone" {
-  description = "List of avaliable zone"
-  type        = list(string)
-}
-
-
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
